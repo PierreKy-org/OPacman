972c2bc0def1f82fbfe6863b4e23d9565ab79d83
Zorrino <pierrekouyoumdjian@hotmail.fr>
2020-11-12 19:22:28 +0100

    Modification des types

--- UF.ml
+++ UF.ml
@@ -1,25 +1,42 @@
 (* Les 2 types utilisables *)
 
-type 't arbre = { valeur : 't; mutable parent : 't arbre option} 
+type 't arbre = { valeur : 't; mutable enfant : 't arbre array option  } 
 
 type  't union_find = 't arbre array ;;
+ 
+(* La fusion met l'arbre 2 sous l'arbre 1 *)
+let fusion a1 a2 = 
+  let rec aux a3 a2 = match a3 with 
+  | {valeur ; enfant } when enfant = None -> a3.enfant <- Some ([|a2|])
+  | {valeur ; enfant } -> let ran = Random.int ((Array.length (Option.get enfant))-1) in  aux (Option.get enfant).(ran) a2
+  in 
+  aux a1 a2;
+  
+
+
+(* Opérateur *)
+
+let ( ?. ) a   = {valeur = a; enfant = None}
+let ( ** ) a b = fusion a b
+
 
 (* Ici c'est un peu le crash test *)
-let arbretest = {valeur = 5; parent = Some  ({valeur = 8 ; parent = None})}
-let arbretest2 = {valeur = 2; parent = Some ({valeur = 3; parent = None}) }
-let arbretest3 = {valeur = 4; parent = None }
+let arbretest = {valeur = 8; enfant = Some  ([|{valeur = 5 ; enfant = None};{valeur = 1 ; enfant = None}|])}
+let arbretest2 = {valeur = 3; enfant = Some ([|{valeur = 2; enfant = None}|]) }
+let arbretest3 = {valeur = 4; enfant = None }
 
-let union = [|arbretest; arbretest2; arbretest3 |]
+let union = [|arbretest; arbretest2; arbretest3|]
 
-(* La fusion met l'arbre 1 sous l'arbre 2 *)
-let rec fusion a1 a2 = match a1 with 
-| {valeur ; parent } when parent = None -> a1.parent <- Some a2;
-| {valeur ; parent } -> fusion (Option.get parent) a2
 
 (* Debut d'un pretty printer *)
+
+(*Refaire le printer_arbre car il est horrible x) *)
 let rec printer_arbre arbre = match arbre with 
-| {valeur ; parent } when parent = None -> print_int valeur
-| {valeur ; parent } -> printer_arbre (Option.get parent); print_string " -> "; print_int valeur
+| {valeur ; enfant } when enfant = None -> print_int valeur;
+| {valeur ; enfant } -> print_int valeur; print_string " -> ";
+                        for i=0 to ((Array.length (Option.get enfant))-1) do
+                          printer_arbre (Option.get enfant).(i); if i = ((Array.length (Option.get enfant))-1) then print_string "" else print_string " | "
+                          done
 
 let printer_union union = 
   print_string ("Votre tableau est de longueur " ^ string_of_int (Array.length union) ^ "\n");
@@ -29,5 +46,6 @@ let printer_union union =
     print_string "\n"
   done
 
-(*Print l'union, puis fusionne l'arbre 0 et l'arbre 1 puis print le résultat *)
-let () =   printer_union union ; fusion union.(0) union.(1); printer_union union; 
+
+(* Print l'union, puis fusionne l'arbre 0 et l'arbre 1 puis print le résultat *)
+let () =   arbretest ** arbretest2; printer_union union; 
