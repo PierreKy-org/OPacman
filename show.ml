f794dea58b584d36a4d9a8984ad6427fd4f7b2fc
Zorrino <pierrekouyoumdjian@hotmail.fr>
2020-11-30 15:36:08 +0100

    V1 pacman avec déplacement

--- show.ml
+++ show.ml
@@ -2,7 +2,37 @@ open Graphics
 open Sys
 open Unix
 (* Graphics *)
-let rec loop () = loop ()
+
+
+
+let case_pacman = ref 0
+
+let trace_pacman uplefty upleftx l h taille_case  = 
+  let x = !case_pacman / l in
+  let y = !case_pacman mod l in
+ (* set_color white;
+  match c with 
+  | w when w = 'z' -> fill_circle ((uplefty + ((taille_case* (y))))-(taille_case/2)) ((upleftx - ((taille_case*(x+1))))+(taille_case/2))
+     ((taille_case/2)-3);
+  | w when w = 'q' ->
+  | w when w = 'd' ->
+  | w when w = 's' -> *)
+     set_color yellow;
+  fill_circle ((uplefty + ((taille_case* (y+1))))-(taille_case/2)) ((upleftx - ((taille_case*(x+1))))+(taille_case/2))
+     ((taille_case/2)-3)
+ 
+let rec loop l = 
+ let c = read_key() in 
+ let x = !case_pacman / l in
+ let y = !case_pacman mod l in
+ match c with 
+   | w when w = 'z' -> case_pacman := (y + l*(x-1)); print_int(!case_pacman) ;print_string("\n"); trace_pacman 200 800 10 10 50 ;loop l
+   | w when w = 'q' -> case_pacman := (y-1) + l*x; print_int(!case_pacman) ; print_string("\n");trace_pacman 200 800 10 10 50 ;loop l
+   | w when w = 'd' -> case_pacman := (y+1) + l*x; print_int(!case_pacman) ; print_string("\n");trace_pacman 200 800 10 10 50 ;loop l
+   | w when w = 's' -> case_pacman := (y + l*(x+1)); print_int(!case_pacman) ; print_string("\n");trace_pacman 200 800 10 10 50 ;loop l
+ 
+
+ 
 
 (* 0 bug elle marche nickel, construit le pourtour du labyrinthe et affiche l'entrée et la sortie du labyrinthe*)
 let trace_pourtour upleftx uplefty l h taille_case = begin 
@@ -15,12 +45,12 @@ let trace_pourtour upleftx uplefty l h taille_case = begin
   lineto (upleftx  + (taille_case * (l) ))  (uplefty - (taille_case * (h)));
   end
 (* Parfait, elle fait son taff, construit le mur demander dans le labyrinthe *)
-let trace_mur upleftx uplefty taille_case (d,x,y) = 
-    moveto (upleftx + (taille_case* (y+1))) (uplefty - (taille_case*(x+1)));
+let trace_mur uplefty upleftx taille_case (d,x,y) = 
+    moveto (uplefty + (taille_case* (y+1))) (upleftx - (taille_case*(x+1)));
     if d = 1 then
-       lineto (upleftx + (taille_case* (y+1))) (uplefty - (taille_case*(x))) 
+       lineto (uplefty + (taille_case* (y+1))) (upleftx - (taille_case*(x))) 
     else 
-            lineto (upleftx + (taille_case* (y))) (uplefty - (taille_case*(x+1)))
+            lineto (uplefty + (taille_case* (y))) (upleftx - (taille_case*(x+1)))
 
 (* Début de trace_lab, trace le labyrinthe en suivant la matrice de mur, trace d'abord les murs puis le pourtour *)
 let trace_lab upleftx uplefty taille_case l h mur_present = begin
@@ -40,10 +70,10 @@ let trace_lab upleftx uplefty taille_case l h mur_present = begin
     trace_pourtour upleftx uplefty l h taille_case;
     end
 
-
 let mur_p = Labyrinthe.gen_lab 10 10
 let () =  trace_lab 200 800 50 10 10 mur_p;
-          loop() 
+          trace_pacman 200 800 10 10 50;
+loop 10 
 
 
 
