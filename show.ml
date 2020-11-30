a6e2922e3dd20d4387f5285e676b2fde005535d0
Zorrino <pierrekouyoumdjian@hotmail.fr>
2020-11-30 17:03:53 +0100

    Ajout de commentaires

--- show.ml
+++ show.ml
@@ -8,13 +8,14 @@ open Unix
 (* La case où le pacman est actuellement *)
 let case_pacman = ref 0
 
+
 (* trace le pacman par rapport à case_pacman *)
 let trace_pacman uplefty upleftx l h taille_case  = 
   let x = !case_pacman / l in
   let y = !case_pacman mod l in
   set_color yellow;
-  fill_circle ((uplefty + ((taille_case* (y+1))))-(taille_case/2)) ((upleftx - ((taille_case*(x+1))))+(taille_case/2))
-     ((taille_case/2)-3)
+  fill_circle ((uplefty + ((taille_case* (y+1))))-(taille_case/2)) ((upleftx - ((taille_case*(x+1))))+(taille_case/2)) ((taille_case/2)-3)
+
 
 (* trace un rond blanc pour effacer l'emplacement précédent de pacman *) 
 let white_pacman uplefty upleftx l h taille_case c = 
@@ -33,39 +34,40 @@ let white_pacman uplefty upleftx l h taille_case c =
    (* Ne fais rien si une autre touche est enfoncée *)
    | w -> case_pacman := !case_pacman 
  
+
+(* Boucle infinie pour afficher le mot "gagné"
+ * On peut mettre un read_key() pour sortir du programme proprement *)
 let rec winner_loop() =
-                 set_color black;
-                moveto 300 450;
-                set_font "-*-fixed-medium-r-semicondensed--150-*-*-*-*-*-iso8859-1" ;
-                draw_string("GAGNE");
-               winner_loop() 
+  set_color black;
+  moveto 300 450;
+  set_font "-*-fixed-medium-r-semicondensed--150-*-*-*-*-*-iso8859-1" ;
+  draw_string("GAGNE");
+  winner_loop() 
             
 
 (* Boucle infinie qui lance l'affichage + la lecture d'entrée clavier *) 
 let rec loop l h = 
  if !case_pacman = l*h-1 then begin
-         clear_graph();
-         winner_loop()
-        end
-        else 
-           
-           
- let c = read_key() in 
- let x = !case_pacman / l in
- let y = !case_pacman mod l in
- (* dans l'ordre :
+   clear_graph();
+   winner_loop()
+   end
+ else                
+   let c = read_key() in 
+   let x = !case_pacman / l in
+   let y = !case_pacman mod l in
+   (* dans l'ordre :
          * - calcule la position du pacman après la pression d'une touche
          * - efface l'ancien pacman 
          * - affiche le nouveau pacman *)
- match c with 
-   | w when w = 'z' -> case_pacman := (y + l*(x-1)); white_pacman 200 800 10 10 50 c; trace_pacman 200 800 10 10 50 ;loop l h
-   | w when w = 'q' -> case_pacman := (y-1) + l*x; white_pacman 200 800 10 10 50 c;trace_pacman 200 800 10 10 50 ;loop l h
-   | w when w = 'd' -> case_pacman := (y+1) + l*x; white_pacman 200 800 10 10 50 c;trace_pacman 200 800 10 10 50 ;loop l h
-   | w when w = 's' -> case_pacman := (y + l*(x+1)); white_pacman 200 800 10 10 50 c;trace_pacman 200 800 10 10 50 ;loop l h
-   (* Permet de quitter le jeu *)
-   | w when w = 'n' -> case_pacman := !case_pacman
+   match c with 
+     | w when w = 'z' -> case_pacman := (y + l*(x-1)); white_pacman 200 800 10 10 50 c; trace_pacman 200 800 10 10 50 ;loop l h
+     | w when w = 'q' -> case_pacman := (y-1) + l*x; white_pacman 200 800 10 10 50 c;trace_pacman 200 800 10 10 50 ;loop l h
+     | w when w = 'd' -> case_pacman := (y+1) + l*x; white_pacman 200 800 10 10 50 c;trace_pacman 200 800 10 10 50 ;loop l h
+     | w when w = 's' -> case_pacman := (y + l*(x+1)); white_pacman 200 800 10 10 50 c;trace_pacman 200 800 10 10 50 ;loop l h
+     (* Permet de quitter le jeu *)
+     | w when w = 'n' -> case_pacman := !case_pacman
    (*Empeche de crash si on appuie sur autre chose *)
-   | w -> loop l h 
+     | w -> loop l h 
 
  
 
@@ -79,26 +81,28 @@ let trace_pourtour upleftx uplefty l h taille_case = begin
   moveto (upleftx  + (taille_case * (l) )) (uplefty - (taille_case * (h-1))); 
   lineto (upleftx  + (taille_case * (l) ))  (uplefty - (taille_case * (h)));
   end
+
+
 (* Parfait, elle fait son taff, construit le mur demander dans le labyrinthe *)
 let trace_mur uplefty upleftx taille_case (d,x,y) = 
     moveto (uplefty + (taille_case* (y+1))) (upleftx - (taille_case*(x+1)));
     if d = 1 then
        lineto (uplefty + (taille_case* (y+1))) (upleftx - (taille_case*(x))) 
     else 
-            lineto (uplefty + (taille_case* (y))) (upleftx - (taille_case*(x+1)))
+       lineto (uplefty + (taille_case* (y))) (upleftx - (taille_case*(x+1)))
+
 
 (* Début de trace_lab, trace le labyrinthe en suivant la matrice de mur, trace d'abord les murs puis le pourtour *)
 let trace_lab upleftx uplefty taille_case l h mur_present = begin
     open_graph " 900x900"; 
     let doNothing = ref 0 in 
-
       for y = 0 to (l-1) do
         for x = 0 to (h-1) do
           if (mur_present.(0).(x).(y)) = true 
-          then trace_mur upleftx uplefty taille_case (0,x,y)
+           then trace_mur upleftx uplefty taille_case (0,x,y)
           else doNothing := !doNothing;
           if (mur_present.(1).(x).(y)) = true 
-          then trace_mur upleftx uplefty taille_case (1,x,y)
+           then trace_mur upleftx uplefty taille_case (1,x,y)
           else doNothing := !doNothing;
           done;
         done;
